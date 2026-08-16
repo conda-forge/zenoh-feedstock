dd7feb94ebde8a14b19bb49b59b03f1ad5581d6b
Silvio Traversaro <silvio.traversaro@gbionics.ai>
2026-08-17 00:18:29 +0200

    Fix license handling

--- recipe/build_zenoh_plugin_storage_manager.sh
+++ recipe/build_zenoh_plugin_storage_manager.sh
@@ -1,3 +1,4 @@
+cargo-bundle-licenses --format yaml --output ${SRC_DIR}/THIRDPARTY.yml
 cargo build --locked --profile release --package zenoh-plugin-storage-manager
 mkdir -p ${PREFIX}/lib
 cp ./target/${CARGO_BUILD_TARGET}/release/libzenoh_plugin_storage_manager${SHLIB_EXT} ${PREFIX}/lib/
